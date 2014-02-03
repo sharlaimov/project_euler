519c6d408ea3056e42bb3521f26f025955129f84
e.sharlaimov <sharlaimov@gmail.com>
2014-02-03 14:55:46 +0200

    Added 006 problem

--- 006_sum_square_difference.py
+++ 006_sum_square_difference.py
@@ -7,7 +7,21 @@
 #(1 + 2 + ... + 10)^2 = 55^2 = 3025
 
 #Hence the difference between the sum of the squares of the first ten natural numbers 
-#and the square of the sum is 3025 ? 385 = 2640.
+#and the square of the sum is 3025 - 385 = 2640.
 
 #Find the difference between the sum of the squares of the first one hundred natural numbers
 #and the square of the sum.
+
+mul1 = range(1, 101)
+mul2 = range(1, 101)
+res = 0
+
+#Квадрат суммы n слагаемых равен сумме их квадратов плюс удвоенная сумма всевозможных попарных произведений
+# этих слагаемых вида a_ia_j, где i<j.
+
+for i in mul1:
+    for j in mul2:
+        if i != j and i < j:
+            res += 2*i*j
+
+print(res)
