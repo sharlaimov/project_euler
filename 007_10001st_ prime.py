21b5ceea0b2d3c7444d73d2adeb99d50a605cb16
e.sharlaimov <sharlaimov@gmail.com>
2014-02-03 15:14:46 +0200

    Added 007 problem

---
+++ 007_10001st_ prime.py
@@ -0,0 +1,26 @@
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+#What is the 10 001st prime number?
+
+
+#bruteforce power
+
+primes = []
+num = 2
+
+
+def is_prime(n):
+    for prime in primes:
+        if n % prime == 0:
+            return False
+    return True
+
+
+while len(primes) != 10001:
+    if is_prime(num):
+        primes.append(num)
+    num += 1
+
+print(primes[10000])
+
+
