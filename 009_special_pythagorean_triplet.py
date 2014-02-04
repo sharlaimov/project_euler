dcde04f8e75dbbdbfb76979674bbac5ca5599641
e.sharlaimov <sharlaimov@gmail.com>
2014-02-04 12:24:18 +0200

    Added 009 problem

--- 009_special_pythagorean_triplet.py
+++ 009_special_pythagorean_triplet.py
@@ -9,3 +9,20 @@ __author__ = 'ESharlaimov'
 
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 #Find the product abc.
+
+import math
+
+squares = []
+
+for i in range(1, 501):
+    squares.append(i**2)
+
+
+print(squares)
+
+for b2 in squares:
+    for a2 in squares[:squares.index(b2)]:
+        if a2+b2 in squares:
+            if squares.index(a2) + squares.index(b2) + squares.index(a2+b2) == 1000:
+                print(squares.index(a2), squares.index(b2), squares.index(a2+b2))
+
