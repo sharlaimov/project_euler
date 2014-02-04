56c6b59e6802faa20496e0bbc3c397d9b37314a7
e.sharlaimov <sharlaimov@gmail.com>
2014-02-04 13:49:56 +0200

    added new solution for 009 problem

--- 009_special_pythagorean_triplet.py
+++ 009_special_pythagorean_triplet.py
@@ -10,18 +10,48 @@ __author__ = 'ESharlaimov'
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 #Find the product abc.
 
-import math
 
+#brute force
 squares = []
 
 for i in range(1, 501):
-    squares.append(i**2)
+    squares.append(i ** 2)
 
 for b2 in squares:
     for a2 in squares:
         if a2 > b2:
             break
-        if a2+b2 in squares:
-            if squares.index(a2)+1 + squares.index(b2)+1 + squares.index(a2+b2)+1 == 1000:
-                print(squares.index(a2)+1, squares.index(b2)+1, squares.index(a2+b2)+1)
+        if a2 + b2 in squares:
+            if squares.index(a2) + 1 + squares.index(b2) + 1 + squares.index(a2 + b2) + 1 == 1000:
+                print(squares.index(a2) + 1, squares.index(b2) + 1, squares.index(a2 + b2) + 1)
 
+
+#http://mathworld.wolfram.com/PythagoreanTriple.html
+
+def is_opposite_parity(a, b):
+    return a % 2 != b % 2
+
+def gcd(a, b):
+    if a < b:
+        gcd(b, a)
+
+    if b == 0:
+        return a
+
+    return gcd(b, a % b)
+
+
+u = 1
+v = 1
+
+while v*(u+v) != 500:
+    if v > u:
+        u += 1
+    else:
+        v += 1
+        u = 1
+
+print(v**2 - u**2, 2 * u * v, v**2 + u**2)
+
+#    if gcd(v, u) == 1 and is_opposite_parity(u, v):
+#       print(v**2 - u**2, 2 * u * v, v**2 + u**2)
